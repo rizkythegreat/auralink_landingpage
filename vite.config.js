10341a6df5deef99509ba90ee8de4fb797901173
Rizky Rahman Salam <clashikik@gmail.com>
2025-12-27 13:30:05 +0700

    fix: remove cloudfare from viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
-import { cloudflare } from '@cloudflare/vite-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react(), cloudflare()]
+  base: '/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
+  plugins: [tailwindcss(), react()]
 });
